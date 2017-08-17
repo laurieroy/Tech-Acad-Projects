48451efd89518c7ef0f3d8f6670aedb0d86e57f0
Laurie Roy <laurieroy.dev@gmail.com>
2017-08-16 19:10:46 -0700

    Range drill in Python3
    simple drills using for, range, and *

---
+++ python/range_drill.py
@@ -0,0 +1,30 @@
+#Drill Python Range Function
+# Python 3.6.2
+# Laurette Roy, Tech Academy, Aug 2017
+# Purpose: exercise in using range and displaying results
+
+#1
+print('\nProblem 1: \n')
+x=4
+for i in range(x):
+    print(i)
+
+
+
+#2
+print('\nProblem 2:\n')
+
+sample =[]
+for i in range(3,-1,-1):
+    sample.append(i)
+print(*sample)
+
+
+#3
+print('\nProblem 3:\n')
+sample3=[]
+for i in range(8,0,-2):
+    sample3.append(i)
+print (*sample3)
+
+
