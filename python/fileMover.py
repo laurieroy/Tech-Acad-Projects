8993ccdabad49ce731f4633a9c66600a5199fdce
Laurie Roy <laurieroy.dev@gmail.com>
2017-08-31 22:37:02 -0700

    Updated fileMover to use glob and copy2

--- python/fileMover.py
+++ python/fileMover.py
@@ -1,57 +1,59 @@
-# fileMover.py
-"""Program copies text files to another directory."""
-# **************************************************************************************
-# Python:   2.7.13
-# Author:   Laurette Roy
-# Purpose:  This program moves all .txt files from one folder to another
-#           with the click of a button. The filepath is printed
-#           to the shell after move. Files are removed from A after move.            
-#            Assumptions: both files are on desktop, both directories exist.
-#           named Folder A, Folder B
-#           Use: 2.7.x, shutil module, run in shell, use IDLE
-# test platform: Win7
-#
-#           This program is an exercise for the python module of the Tech Academy bootcamp, Aug 2017.
-# ***************************************************************************************
-import tkinter
+# def fileMover():
+"""Program moves text files to another directory."""
+    # **************************************************************************************
+    # Python:   2.7.13
+    # Filename: fileMover.py
+    # Author:   Laurette Roy
+    # Purpose:  This program moves all .txt files from one folder to another
+    #           with the click of a button (F5). The filepath is printed
+    #           to the shell after move. Files are removed from A after move.            
+    #            Assumptions: both folders are on desktop, & both directories exist.
+    #           named Folder A, Folder B
+    #            Use: 2.7.x, shutil module, run in shell, use IDLE
+    # test platform: Win7
+    #
+    #           This program is an exercise for the python module of the Tech Academy bootcamp, Aug 2017.
+    # ***************************************************************************************
 import shutil
 import os
-
-# display in gui files in dir A, dir B.
-# move file xyz
-# make GUI with button to move files or
-# Ask user if ready to move files from Folder A to Fodler B 'y'<enter>
-
+import glob
 source = 'c:/Users/Student/Desktop/Folder A'
 destination = 'c:/Users/Student/Desktop/Folder B'
+def start():
+    os.chdir(source)
+    print "Source directory is: ", os.getcwd()   # Would you like to change directories?"
+    print "The contents of ", source, 'are: \n', os.listdir(source)
+    print "The contents of ", destination, 'are: \n', os.listdir(destination)
+    confirm()
 
-print os.getcwd()
-os.chdir(source)
-print os.getcwd()
-# moveFiles = confirm()
-print("Type confirm() <enter> to start dialogue.")
 def confirm():
     print("Are you sure you'd like to move the contents of Folder A to Folder B?")
-    answer= raw_input("\n y/n ")
+    answer = raw_input("\n y/n ")
     choice(answer)
+    
 def choice(answer):
     if answer == 'y':   # if choose to copy files, proceed
-        fileMove()
-        
-            
+        fileMove()      
     elif answer == 'n':     #if they choose no, quit program
         pass
-               
     else:
         print("\nYour choices are 'y' for 'YES', or 'n' for 'NO'. ")
         confirm()
 
-
 def fileMove():
     try:
-        print("The contents of ", source, 'are: \n', os.listdir(source))
-        for files in source:
-            if files.endswith(".txt"):
-                shutil.move(files,destination)
-    except:
-        print(shutil.Error)
+        files = os.listdir(source)
+        print "The contents of ", source, 'are: \n', files 
+        for f in glob.iglob(os.path.join(source, "*.txt")):
+            if os.path.isfile(f):
+                print f
+                shutil.copy2(f, destination)
+                os.remove(f)
+        print "The contents of ", source, 'are now: \n', os.listdir(source)
+        print "The contents of ", destination, 'are now: \n', os.listdir(destination)
+    except Exception, e:
+        print repr(e)
+
+        
+if __name__=='__main__':
+    start()
