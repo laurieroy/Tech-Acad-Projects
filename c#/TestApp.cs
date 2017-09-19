e979f35bdc0dbd48bf9cce11ed31aed2f6b9864e
Laurie Roy <laurieroy.dev@gmail.com>
2017-09-19 13:12:21 -0700

    sample console compile

---
+++ c#/TestApp.cs
@@ -0,0 +1,10 @@
+// A simple C# application. Item 9 TA c# class from  Pro C# 2008 book
+using System;
+
+class TestApp
+{
+   static void Main()
+   {
+      Console.WriteLine("Testing! 1, 2, 3");
+   }
+}
